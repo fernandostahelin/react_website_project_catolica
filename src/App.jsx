e5ca450e9463ef1ab9a543453106bf35778fe70a
Fernando Stahelin <fernandostahelin@gmail.com>
2024-11-07 20:15:21 -0300

    feat: formulario agenda 08

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
-import { BrowserRouter as Router } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import { BrowserRouter as Router } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Form from "./components/Form";
+import './App.css';
 
 function App() {
-  return (
-    <Router>
-      <div className="app">
-        <Header />
-        <main className="main-content">
-          <div className="App">
-      <div className="images">
-        <img src="https://turismo.balneariobarradosul.sc.gov.br/uploads/sites/533/2010/01/9-1000x1000.png"/>
-        
-      </div>
-      <h2>Pronto, você conheceu tudo. A cidade é minúscula mesmo.</h2>
-     </div>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+  return (  
+      <Router>
+        <div className="App">
+          <Header />
+          <main className="main-content">
+            <div className="images">
+              <img src="https://turismo.balneariobarradosul.sc.gov.br/uploads/sites/533/2010/01/9-1000x1000.png" alt="Balneário Barra do Sul"/>
+            </div>
+            <h2>Conheça a praia mais perto de Joinville.</h2>
+            <Form />
+          </main>
+          <Footer />
+        </div>
+      </Router>
   );
 }
 
